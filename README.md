386ebea9a2250cb801afb56f1d4e5eb258428ebd
Bryan Tong <contact@nullivex.com>
2015-12-24 02:50:09 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# bitcoind-watchdog
-A simple NodeJS script that will watchdog your bitcoind and recycle it.
+bitcoind-watchdog
+========
+
+NodeJS process designed to run on top of (ndt)[https://github.com/nullivex/ndt]
+
+## Installation
+
+We assume you already have bitcoind installed and available on your system.
+
+We also assume you already have node and npm installed and available.
+
+Next we want daemontools to run ndt.
+
+On Debian (adapt as neccessary)
+```
+$ apt-get install daemontools daemontools-run
+$ ln -s /etc/service /service
+```
+
+Now we need ndt and bitcoind-watchdog
+
+```
+$ npm -g install ndt bitcoind-watchdog
+```
+
+
+
+## Changelog
+
+### 0.1.0
+* Initial Release
---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict';
+var infant = require('infant')
+var program = require('commander')
+
+var pkg = require('./package.json')
+
+program
+  .version(pkg.version)
+  .option('-i, --interval <n>','Interval in seconds to restart process')
+  .parse(process.argv)
+
+
+var errorHandler = function(err){
+  console.log(err,err.trace)
+  process.exit(1)
+}
+
+var child = infant.parent('bitcoind')
+
+//wire up io
+process.stdin.pipe(child.cp.stdin)
+child.cp.stdout.pipe(process.stdout)
+child.cp.stderr.pipe(process.stderr)
+
+//start bitcoind
+child.start(function(err){
+  if(err) return errorHandler(err)
+  console.log('Bitcoind running')
+})
+
+//setup our restart function
+var restartBitcoind = function(done){
+  child.restart(function(err){
+    done(err)
+  })
+}
+
+//check for an interval and setup a restart if we want
+var interval = program.interval || null
+if(interval){
+  setInterval(function(){
+    restartBitcoind(errorHandler)
+  },interval)
+}
